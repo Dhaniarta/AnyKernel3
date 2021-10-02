e7b79d61f8b6bb15d713a3c4746bd6ff418e8880
Tiery Dhaniarta <90048051+Dhaniarta@users.noreply.github.com>
2021-10-02 11:48:26 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Kitsune Kernel by Dhaniarta @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
+device.name1=whyred
 device.name2=toro
 device.name3=toroplus
 device.name4=tuna
 device.name5=
-supported.versions=
+supported.versions=9-11
 supported.patchlevels=
 '; } # end properties
 
